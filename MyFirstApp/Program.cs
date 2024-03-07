8f053fe8378dab5b60040e48ae573d3680f67583
Kristjan. <kristjan.seli@tktk.ee>
2024-03-07 18:58:41 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment to my code
+
+//rakendus kysib kasutajal sisestada nimi
+//rakendus tervitab kasutajat
+
+Console.WriteLine("Enter your name"); //Output
+//string-Sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+Console.WriteLine($"Hello, {userName} !"); //Output
